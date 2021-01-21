638c9342293c2e7ddd1ee60e505637a3d1b14a38
Pedro <pedroberorato@gmail.com>
2021-01-20 21:42:42 -0300

    Funcionando

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const app = require('express')();
+const express = require('express');
+//Inicia o App
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
+
+//Libera a importação do CSS
+app.use(express.static(__dirname + '/public'));
 
 server.listen(port, () => {
     console.log('Server Running...')
@@ -12,29 +17,26 @@ app.get('/', (req,res) => {
 });
 
 app.get('/:room', (req,res) => {
-    io.on('connection', (socket) => {
     
-        socket.on('join', (data) => {
-            socket.join(data.room);
-            io.in(data.room).emit('message', `New user joined ${data.room} room!`);
-        })
-    
-        socket.on('message', (data) =>{
-            console.log(`message: ${data.msg}`);
-            io.in(data.room).emit('message', data);
-        });  
-    
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-            io.emit('message', 'user disconnected');
-        });
-    });
 
     res.sendFile(__dirname + '/public/room.html');
     
-
 });
 
+io.on('connection', (socket) => {
+    
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        io.in(data.room).emit('message', {msg: `New user joined ${data.room} room!`,  name: 'server'});
+    })
 
+    socket.on('message', (data) =>{
+        console.log(`message: ${data.msg}`);
+        io.in(data.room).emit('message', data);
+    });  
 
-
+    socket.on('disconnect', (data) => {
+        console.log('user disconnected', data);
+        io.emit('message', {msg: 'user disconnected', name: 'server'});
+    });
+});
